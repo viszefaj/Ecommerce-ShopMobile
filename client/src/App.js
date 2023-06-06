cb55f1ce795f8b0f0af300d47def3577deeb83c0
Egi Karakashi <e.karakashi.ext@teamsystem.com>
2023-06-06 22:55:00 +0200

    change routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-//pages
 import { Home, Contact, Login, Register, Reset } from "./pages/";
-//components
 import { Header, Footer } from "./components/index";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Data from "./components/FlashDeals/Data";
 import Admin from "./pages/admin/Admin";
-import Dashboard from "./components/dashboard/dashboard";
 import Cart from "./pages/cart/Cart";
 import Checkout from "./pages/checkout/Checkout";
+import ProductsLogs from "./pages/admin/ProductsLogs";
+import MessageTable from "./pages/admin/MessagesTable";
+import UserManagement from "./pages/admin/UserManagement";
+import OrderLogs from "./pages/admin/OrdersLogs";
 
 function App() {
-  const [setFirst] = useState("Dionis");
-
   return (
     <>
       <BrowserRouter>
@@ -31,6 +28,10 @@ function App() {
           <Route path="/admin" element={<Admin />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
+          <Route path="/items" element={<ProductsLogs />} />
+          <Route path="/messages" element={<MessageTable />} />
+          <Route path="/orders" element={<OrderLogs />} />
+          <Route path="/users" element={<UserManagement />} />
         </Routes>
 
         <Footer />
